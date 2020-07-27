f4898d01efa067a5cf43948a8d61d25bc346bf01
jeongha-joo <jha970107@gmail.com>
2020-07-28 04:54:07 +0900

    STT

--- app/src/main/java/com/example/speak_5/AddActivity.java
+++ app/src/main/java/com/example/speak_5/AddActivity.java
@@ -1,30 +1,51 @@
 package com.example.speak_5;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.speak_5.MainActivity;
+import com.example.speak_5.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class AddActivity extends AppCompatActivity {
 
     EditText editText;
-    Button btnback;
-    Button btnsave;
+    Button btnback;    Button btnsave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_add);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
 
-            Setting();
-            AddListener();
+        Setting();
+        AddListener();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "저장할 내용을 말씀해주세요.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                VoiceTask voiceTask = new VoiceTask();
+                voiceTask.execute();
+            }
+        });
     }
     void Setting(){
         editText = findViewById(R.id.edtMemo);
@@ -36,7 +57,7 @@ public class AddActivity extends AppCompatActivity {
         btnback.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 intent.putExtra("main", editText.getText());
                 startActivity(intent);
             }
@@ -48,7 +69,7 @@ public class AddActivity extends AppCompatActivity {
 
                 if(str.length() > 0){
                     Date date = new Date();
-                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
                     String substr = sdf.format(date);
                     Toast.makeText(AddActivity.this, "저장 되었습니다.", Toast.LENGTH_SHORT).show();
@@ -61,5 +82,61 @@ public class AddActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public class VoiceTask extends AsyncTask<String, Integer, String> {
+        String str = null;
+
+        @Override
+        protected String doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            try {
+                getVoice();
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+            return str;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+
+            } catch (Exception e) {
+                Log.d("onActivityResult", "getImageURL exception");
+            }
+        }
+    }
+
+    private void getVoice() {
+
+        Intent intent = new Intent();
+        intent.setAction(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        String language = "ko-KR";
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language);
+        startActivityForResult(intent, 2);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            ArrayList<String> results = data
+                    .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            String str = results.get(0);
+            Toast.makeText(getBaseContext(), str, Toast.LENGTH_SHORT).show();
+
+            TextView tv = findViewById(R.id.edtMemo);
+            tv.setText(str);
+        }
     }
 }
